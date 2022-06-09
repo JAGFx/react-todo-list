413bea69e698984231441e5b06aeb0f105934598
SMITH Emmanuel <contact@jagfx.fr>
2022-06-09 21:30:41 +0200

    :recycle: refactor utils

---
+++ src/utils/generator.utils.js
@@ -0,0 +1,12 @@
+/**
+ * @author:  Emmanuel SMITH <hey@emmanuel-smith.me>
+ * project:  react-learn-3
+ * file:  generator.utils.js
+ * Date:  09/06/2022
+ * Time:  21:29
+ */
+
+export const generateUuid = () =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
--- src/utils/todo.utils.js
+++ src/utils/todo.utils.js
@@ -6,11 +6,7 @@
  * Time:  19:17
  */
 import { STATE } from '@/store/todo.store';
-
-export const generateUuid = () =>
-  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-  );
+import { generateUuid } from '@/utils/generator.utils';
 
 export const generateTodoObject = (text, uuid = null, tags = [], state = STATE.TODO) => {
   return {
