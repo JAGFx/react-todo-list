e1e9f5165c459476a45aa71c2be7b766eefb6837
SMITH Emmanuel <contact@jagfx.fr>
2022-06-13 21:11:32 +0200

    :white_check_mark: add unit test for filter todo

--- src/Todo/todo.utils.test.js
+++ src/Todo/todo.utils.test.js
@@ -7,7 +7,23 @@
  */
 
 import { STATE } from '@/Todo/todo.constantes.js';
-import { generateTodoObject } from '@/Todo/todo.utils.js';
+import {
+  applyFiltersOnTodoList,
+  generateTodoObject
+} from '@/Todo/todo.utils.js';
+
+const generateTodoForTest = (
+  text = 'my text',
+  uuid = 'my-uuid',
+  tags = [],
+  state = STATE.TODO
+) => {
+  return { text, uuid, tags, state };
+};
+
+const generateFilter = (search = '', tags = [], showCompleted = true) => {
+  return { search, tags, showCompleted };
+};
 
 describe('Todo utils', () => {
   const dataForGenerationTodoObject = [null, 'my_uuid'];
@@ -21,7 +37,7 @@ describe('Todo utils', () => {
 
       expect(typeof generatedTodoObject).toBe('object');
       expect(generatedTodoObject).toMatchObject({ text, tags, state });
-      expect(Object.hasOwn(generatedTodoObject, 'uuid')).toBeTruthy();
+      expect(generatedTodoObject).toHaveProperty('uuid');
 
       if (uuid === null) {
         expect(typeof generatedTodoObject.uuid).toBe('string');
@@ -30,4 +46,91 @@ describe('Todo utils', () => {
       }
     }
   );
+});
+
+describe('Apply filters', () => {
+  const wrongFiltersData = [
+    {},
+    { search: '' },
+    { search: '', tags: [''] },
+    { toto: false }
+  ];
+
+  test.each(wrongFiltersData)(
+    'An error must be thrown with invalid filters data',
+    (wrongFilter) => {
+      expect(() =>
+        applyFiltersOnTodoList([generateTodoForTest()], wrongFilter)
+      ).toThrow(/entry was not found on todo filters/);
+    }
+  );
+
+  const todoForFieldMatches = [
+    generateTodoForTest('my text'),
+    generateTodoForTest('My Text'),
+    generateTodoForTest('another text', 'uuid', ['tag'], STATE.DONE),
+    generateTodoForTest('awesome text', 'uuid2', ['tag1'])
+  ];
+  const todoTextPropertySearch = [
+    generateFilter('my text'),
+    generateFilter('MY TEXT'),
+    generateFilter('mY tEXT')
+  ];
+
+  test.each(todoTextPropertySearch)(
+    'Filter todo list must not be case sensitive on text field',
+    (filter) => {
+      const resultFilter = applyFiltersOnTodoList(todoForFieldMatches, filter);
+
+      expect(resultFilter).toHaveLength(2);
+      resultFilter.map((todo) =>
+        expect(todo.text.toLowerCase()).toEqual('my text')
+      );
+    }
+  );
+
+  test('Filter todo list must contain tag', () => {
+    const resultFilter = applyFiltersOnTodoList(
+      todoForFieldMatches,
+      generateFilter('', ['tag'])
+    );
+
+    expect(resultFilter).toHaveLength(1);
+    expect(resultFilter[0]).toMatchObject(
+      generateTodoForTest('another text', 'uuid', ['tag'], STATE.DONE)
+    );
+  });
+
+  test('Filter todo list must return only uncompleted todo', () => {
+    const resultFilter = applyFiltersOnTodoList(
+      todoForFieldMatches,
+      generateFilter('my text', [], false)
+    );
+
+    expect(resultFilter).toHaveLength(2);
+    resultFilter.map((todo) =>
+      expect(todo.text.toLowerCase()).toEqual('my text')
+    );
+  });
+
+  test('Filter todo list must match with multi parameters', () => {
+    const resultFilter = applyFiltersOnTodoList(
+      todoForFieldMatches,
+      generateFilter('awesome text', ['tag1'], false)
+    );
+
+    expect(resultFilter).toHaveLength(1);
+    expect(resultFilter[0]).toMatchObject(
+      generateTodoForTest('awesome text', 'uuid2', ['tag1'])
+    );
+  });
+
+  test('No tag must be return with no matching filters', () => {
+    const resultFilter = applyFiltersOnTodoList(
+      todoForFieldMatches,
+      generateFilter('unicorn world', ['bisounours'], false)
+    );
+
+    expect(resultFilter).toHaveLength(0);
+  });
 });
