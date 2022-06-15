0b3bedd833896e76cb7284a09d207bda5ac21dc1
JAGFx <contact@jagfx.fr>
2022-06-15 20:07:51 +0200

    :green_heart: correct assets production build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '',
   resolve: {
-    alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }]
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  },
+  build: {
+    rollupOptions: {}
   }
 });
