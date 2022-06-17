e16d28b50d8de58aaf7c1ded409144e27d1f7c80
JAGFx <contact@jagfx.fr>
2022-06-17 13:37:46 +0200

    :bug: add config for dev and production preview

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://jagfx.github.io/react-todo-list/',
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url))
-      }
-    ]
-  },
-  build: {
-    rollupOptions: {}
-  }
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASE_URL,
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('./src', import.meta.url))
+        }
+      ]
+    }
+  });
+};
